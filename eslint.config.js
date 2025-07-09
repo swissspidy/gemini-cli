e2b91b4ee5da6545629487fe45d96f47d484e994
Pascal Birchler <pascalb@google.com>
2025-07-09 10:15:28 +0200

    Add missing trailing commas

--- eslint.config.js
+++ eslint.config.js
@@ -114,8 +114,8 @@ export default tseslint.config(
       'import/no-internal-modules': [
         'error',
         {
-          allow: [ 'react-dom/test-utils', 'memfs/lib/volume.js', 'yargs/**']
-        }
+          allow: ['react-dom/test-utils', 'memfs/lib/volume.js', 'yargs/**'],
+        },
       ],
       'import/no-relative-packages': 'error',
       'no-cond-assign': 'error',
@@ -210,5 +210,5 @@ export default tseslint.config(
         },
       ],
     },
-  }
+  },
 );
